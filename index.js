c49f803d8bc103b33dedcc2884c7a5774b7675c2
jongyunjeong <jongyunj@icloud.com>
2020-06-11 16:39:07 +0900

    express, babel, middleware

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hi!");
+import express from "express"; //const express = require("express");
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+const app = express();
+const port = 4000;
+
+const handleProfile = (req, res) => res.send("you are my profile");
+
+app.unsubscribe(cookieParser());
+app.unsubscribe(bodyParser.json());
+app.unsubscribe(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/profile", handleProfile);
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
