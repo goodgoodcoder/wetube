0a7b14530d19321e225bc7f6ba5b03b6df47396f
jongyunjeong <jongyunj@icloud.com>
2020-06-22 10:56:20 +0900

    MongoDB, Mongoose, DotEnv, Models

--- init.js
+++ init.js
@@ -1,6 +1,11 @@
 import app from "./app";
+import "./db";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
+import "./models/Comment";
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`✅ Listening on : http://localhost:${port}`);
---
+++ models/Video.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+  fileUrl: {
+    type: String,
+    required: "File URL is required", // file URL이 없는 경우 Error Message
+  },
+  title: {
+    type: String,
+    required: "Title is required",
+  },
+  description: String,
+  views: {
+    tupe: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  comment: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+const model = mongoose.model("Video", VideoSchema);
+export default model;
